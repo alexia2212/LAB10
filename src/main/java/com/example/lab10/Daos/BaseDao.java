d5a2b2dead569fa98768e44e90544648f2613a8a
Alexia Jauregui <alexia_jg@outlook.es>
2022-11-16 20:44:19 -0500

    aaaaa

---
+++ src/main/java/com/example/lab10/Daos/BaseDao.java
@@ -0,0 +1,19 @@
+package com.example.lab10.Daos;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class BaseDao {
+    public Connection getConnection() throws SQLException {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        }catch (ClassNotFoundException e){
+            throw new RuntimeException(e);
+        }
+        String user = "root";
+        String pass = "123456";
+        String url = "jdbc:mysql://localhost:3306/bi_corp_business";
+        return DriverManager.getConnection(url, user, pass);
+    }
+}

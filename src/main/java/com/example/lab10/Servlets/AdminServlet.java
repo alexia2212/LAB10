5c9594c437ea9801f448729c803502e814d5de2a
AnaisSalvador <anaissalvadorrodriguez@gmail.com>
2022-11-17 09:11:45 -0500

    AdminServlet crear2

--- src/main/java/com/example/lab10/Servlets/AdminServlet.java
+++ src/main/java/com/example/lab10/Servlets/AdminServlet.java
@@ -1,27 +1,27 @@
 package com.example.lab10.Servlets;
 
+import com.example.lab10.Beans.Contratos;
+import com.example.lab10.Beans.Clientes;
+import com.example.lab10.Beans.Credentials;
+import com.example.lab10.Daos.BaseDao;
+import com.example.lab10.Daos.ClienteDao;
+import com.example.lab10.Daos.AdminDao;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.io.*;
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "helloServlet", value = "/hello-servlet")
+@WebServlet(name = "AdminServlet", value = {"/AdminServlet",""})
 public class AdminServlet extends HttpServlet {
-    private String message;
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    public void init() {
-        message = "Hello World!";
     }
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        // Hello
-        PrintWriter out = response.getWriter();
-        out.println("<html><body>");
-        out.println("<h1>" + message + "</h1>");
-        out.println("</body></html>");
-    }
-
-    public void destroy() {
     }
 }

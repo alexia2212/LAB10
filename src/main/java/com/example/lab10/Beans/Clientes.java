032a3015bb655a4307fe79aa96087561d355ece5
Anais <victor.bolivar@pucp.edu.pe>
2022-11-16 20:43:15 -0500

    beans

--- src/main/java/com/example/lab10/Beans/Clientes.java
+++ src/main/java/com/example/lab10/Beans/Clientes.java
@@ -1,4 +1,40 @@
 package com.example.lab10.Beans;
 
 public class Clientes {
+    private String nombreCliente;
+    private String edad;
+    private String tipoCliente;
+    private String numeroDocumento;
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getTipoCliente() {
+        return tipoCliente;
+    }
+
+    public void setTipoCliente(String tipoCliente) {
+        this.tipoCliente = tipoCliente;
+    }
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
 }
--- src/main/java/com/example/lab10/Beans/Credentials.java
+++ src/main/java/com/example/lab10/Beans/Credentials.java
@@ -1,4 +1,22 @@
 package com.example.lab10.Beans;
 
 public class Credentials {
+    private String numeroDocumento;
+    private String tipoUsuario;
+
+    public String getNumeroDocumento() {
+        return numeroDocumento;
+    }
+
+    public void setNumeroDocumento(String numeroDocumento) {
+        this.numeroDocumento = numeroDocumento;
+    }
+
+    public String getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
 }
